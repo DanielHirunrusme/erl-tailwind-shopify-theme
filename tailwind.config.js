3907dc8a859ba408c1b426f26834d48179f4bd93
Daniel Hirunrusme <daniel@halfhalf.us>
2024-02-09 13:55:19 -0700

    set viewport height fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,5 +42,17 @@ module.exports = {
       black: '#000000',
     },
   },
+  methods: {
+    setViewHeight: function () {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    },
+  },
+  mounted: function () {
+    this.setViewHeight();
+    window.addEventListener('resize', () => {
+      this.setViewHeight();
+    });
+  },
   plugins: [],
 };
